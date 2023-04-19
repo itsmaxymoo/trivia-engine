1142dae358e70e6bc53b11e739c0d6c02cd8e3c5
itsmaxymoo <max.loiacono@protonmail.com>
2023-04-19 15:11:26 -0400

    bubble click event

--- src/routes/StartButton.svelte
+++ src/routes/StartButton.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
 	export let selectedTopics: Array<String> = [];
+	export let isLoading: boolean = false;
 </script>
 
 <p class="has-text-centered">
 	<button
+		on:click
 		class="button is-medium is-link {selectedTopics.length == 0
 			? 'is-outlined is-static'
-			: ''}"
+			: ''} {isLoading ? 'is-loading' : ''}"
 	>
 		{#if selectedTopics.length == 0}
 			Select topic(s) to start
