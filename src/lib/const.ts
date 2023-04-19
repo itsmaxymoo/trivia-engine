5e9525f3b2a828e72f6409923e30d1a227a05e7b
itsmaxymoo <max.loiacono@protonmail.com>
2023-04-18 20:34:21 -0400

    dynamically load topics

--- src/lib/const.ts
+++ src/lib/const.ts
@@ -1,1 +1,2 @@
 export let appName: string = "Max's Trivia Engine"
+export let questionPath: string = "/questions"
--- src/routes/TopicList.svelte
+++ src/routes/TopicList.svelte
@@ -1,13 +1,41 @@
 <script lang="ts">
-	let loaded: Boolean = false;
+	import { questionPath } from "$lib/const";
+
+	let topicList: Array<String> = [];
+	export let selectedTopics: Array<String> = [];
+
+	async function loadTopicsList() {
+		const res = await fetch(questionPath + "/_topics.json");
+		const resp = await res.text();
+
+		if (res.ok) {
+			topicList = JSON.parse(resp);
+			return true;
+		} else {
+			throw new Error(resp);
+		}
+	}
+
+	let topicsPromise = loadTopicsList();
 </script>
 
 <div class="box">
 	<h2 class="is-size-3 is-underlined">Topics</h2>
 
-	{#if loaded}
-		loaded
-	{:else}
-		<button class="button is-fullwidth is-loading is-link is-medium"></button>
-	{/if}
+	{#await topicsPromise}
+		<button class="button is-fullwidth is-loading is-link is-medium" />
+	{:then}
+		<table class="table is-fullwidth is-striped">
+			<tbody>
+				{#each topicList as topic}
+					<tr class="{(selectedTopics.includes(topic)) ? "is-selected" : ""}">
+						<td>
+							<input type="checkbox" class="checkbox" bind:group={selectedTopics} value="{topic}"/>
+							{topic}
+						</td>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
+	{/await}
 </div>
