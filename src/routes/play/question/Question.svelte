3cc41107f92c1cf2a1a326e3234c7526d1aefe2c
itsmaxymoo <max.loiacono@protonmail.com>
2023-04-24 13:25:26 -0400

    inform user of wright or wrongness

--- src/routes/play/question/Question.svelte
+++ src/routes/play/question/Question.svelte
@@ -2,6 +2,7 @@
 	import { QuestionState, type Question } from "$lib/game";
 	import Answers from "./Answers.svelte";
 	import QuestionText from "./QuestionText.svelte";
+	import Result from "./Result.svelte";
 
 	export let question: Question = {
 		text: "NULL",
@@ -22,10 +23,10 @@
 
 <QuestionText bind:text={question.text} />
 
-<Answers bind:answers={shuffledAnswers} on:answersSelected />
-
-{#if questionAnswerState == QuestionState.TRUE}
-	true
+{#if questionAnswerState == QuestionState.UNANSWERED}
+	<Answers bind:answers={shuffledAnswers} on:answersSelected />
+{:else if questionAnswerState == QuestionState.TRUE}
+	<Result {question} correct={true} />
 {:else if questionAnswerState == QuestionState.FALSE}
-	false
+<Result {question} correct={false} />
 {/if}
---
+++ src/routes/play/question/Result.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import type { Question } from "$lib/game";
+
+	export let correct: boolean = true;
+	export let question: Question = {
+		text: "NULL",
+		answer: "NULL",
+		falseAnswers: [],
+	};
+</script>
+
+<article class="message {correct ? 'is-success' : 'is-danger'}">
+	<div class="message-header">
+		<p>{correct ? "Correct!" : "Wrong!"}</p>
+	</div>
+	<div class="message-body">
+		{#if correct}
+			Congratulations! The correct answer is indeed {question.answer}.
+		{:else}
+			Try again next time! The correct answer is <b>{question.answer}</b>.
+		{/if}
+	</div>
+</article>
