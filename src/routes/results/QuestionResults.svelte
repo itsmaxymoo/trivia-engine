73e0057f463572b9d466554194e70bd30ee38107
itsmaxymoo <max.loiacono@protonmail.com>
2023-04-25 12:55:20 -0400

    show results after the end of the game

--- src/routes/results/QuestionResults.svelte
+++ src/routes/results/QuestionResults.svelte
@@ -1,20 +1,36 @@
 <script lang="ts">
-	import type { QuestionQueueEntry } from "$lib/game";
+	import { QuestionState, type QuestionQueueEntry } from "$lib/game";
 
 	export let questions: Array<QuestionQueueEntry> = [];
 </script>
 
-<table class="table is-fullwidth is-hoverable is-striped">
-	<tbody>
-		{#each questions as question, i}
-			<tr>
-				<td class="has-text-right">
-					{i + 1}
-				</td>
-				<td class="has-text-right">
-					{question.question.text}
-				</td>
+<div class="box">
+	<table class="table is-fullwidth is-striped">
+		<thead>
+			<tr class="has-text-weight-semibold">
+				<td>Number</td>
+				<td>Question</td>
+				<td>Your Answer</td>
 			</tr>
-		{/each}
-	</tbody>
-</table>
+		</thead>
+		<tbody>
+			{#each questions as question, i}
+				<tr>
+					<td class="has-text-right">
+						{i + 1}
+					</td>
+					<td>
+						{question.question.text}
+					</td>
+					<td class="has-text-left">
+						{#if question.state == QuestionState.TRUE}
+							&#129001; Correct!
+						{:else}
+							&#128997; Wrong :P
+						{/if}
+					</td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
+</div>
